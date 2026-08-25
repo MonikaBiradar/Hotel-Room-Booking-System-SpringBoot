e9db08cc8c7d14c4db78b4bcda8492b58e63bbc3
MonikaBiradar <monika.biradar01@gmail.com>
2026-08-25 16:03:04 +0530

    Improve JPA relationships

--- src/main/java/com/monika/hotelroombookingsystem/Booking.java
+++ src/main/java/com/monika/hotelroombookingsystem/Booking.java
@@ -3,9 +3,11 @@ package com.monika.hotelroombookingsystem;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -20,10 +22,12 @@ public class Booking {
     
     private int numOfNights;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_num")
     private Room room;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
     private Customer customer;
     
     
